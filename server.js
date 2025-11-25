93176ea1a0aca7c46ecb958f4fa0fb7a4d4c3470
VladMihai23 <vlamivox@yahoo.com>
2025-11-25 16:31:41 +0200

    first commit

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const port = 3000
+
+app.set('view engine', 'ejs')
+
+app.get('/', (req, res) => {
+  console.log('testing')
+  res.render('index');
+})
+
+app.get('/index', (req, res) => {
+  res.render('index');
+})
+
+app.get('/navbar', (req, res) => {
+  res.render('navbar');
+})
+
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
